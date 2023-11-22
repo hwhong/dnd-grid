cf7b56e40b17ae926d641e803c6ca7fe8afe968b
Warren Hong <hwhong1129@gmail.com>
2023-11-21 22:18:44 -0800

    fix: mousedown is better?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,57 @@
+"use client";
+import classNames from "classnames";
 import styles from "./page.module.css";
+import { useRef, useState } from "react";
 
 export default function Home() {
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [start, setStart] = useState({ x: 0, y: 0 });
+  const [delta, setDelta] = useState({ x: 0, y: 0 });
+  const eleRef = useRef(null);
+
+  const onMouseDown = (e: any) => {
+    setStart({ x: e.clientX, y: e.clientY });
+    setIsMouseDown(true);
+  };
+
+  const onMouseMove = (e: any) => {
+    if (!isMouseDown) {
+      return;
+    }
+
+    const currX = e.clientX;
+    const currY = e.clientY;
+
+    setDelta({ x: currX - start.x, y: currY - start.y });
+  };
+
+  const onMouseUp = () => {
+    setIsMouseDown(false);
+  };
+
   return (
     <div className={styles.root}>
       <div className={styles.grid}>
-        <div className={styles.content}>content</div>
+        <div
+          draggable
+          ref={eleRef}
+          className={classNames(styles.content, styles.gridItem)}
+          // onDrag={onDrag}
+          // onDragStart={onDragStart}
+          style={{
+            transform: `translate(${delta.x}px, ${delta.y}px)`,
+          }}
+          // onDragEnd={onDragEnd}
+          onMouseDown={onMouseDown}
+          onMouseMove={onMouseMove}
+          onMouseUp={onMouseUp}
+        >
+          content
+        </div>
       </div>
     </div>
   );
 }
+
+// use translate to position the items
+// grid items have: position absolute, left: 0, top: 0
